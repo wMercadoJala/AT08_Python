5847f3b9fe3fa063c55d3034f902bfc369de68e6
DennisMont <dmmv429@gmail.com>
2019-01-22 10:34:44 -0400

    Identify if a number is odd or even

---
+++ dennis/Pr_EvenOrOdd.py
@@ -0,0 +1,9 @@
+def even_or_odd(num):
+    if num % 2 == 0: print("number "+str(num)+" is even")
+    else: print("number "+str(num)+" is odd")
+
+
+even_or_odd(3)
+even_or_odd(22)
+even_or_odd(75)
+even_or_odd(40)
