0357bfacdbc91b071d0379a1ea0686a9c51a20a9
geremygustavo <abelmallcu123@gmail.com>
2019-01-22 23:31:01 -0400

    operators

---
+++ abel/practice1/operators.py
@@ -0,0 +1,42 @@
+def add(num1, num2):
+    print(int(num1) + int(num2))
+
+
+def subtract(num1, num2):
+    print(int(num1) - int(num2))
+
+
+def multiplication(num1, num2):
+    print(int(num1) * int(num2))
+
+
+def division(num1, num2):
+    print(int(num1) / int(num2))
+
+
+def mod(num1, num2):
+    print(int(num1) % int(num2))
+
+
+def exp(num1, num2):
+    print(int(num1) ** int(num2))
+
+
+def exp2(num1, num2):
+    print(float(num1) // float(num2))
+
+
+print("sum")
+add(2, 3)
+print("rest ")
+subtract(5, 3)
+print("multiplication")
+multiplication(2, 3)
+print("division")
+division(9, 3)
+print("mod")
+mod(25, 2)
+print("exp")
+exp(2, 3)
+print("exp2")
+exp2(-11, 3)
