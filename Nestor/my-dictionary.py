d3d706cf20aa68a791f172c8ba2eac33e0cb3fc8
estigmata <nestor.otondo@fundacion-jala.org>
2019-01-29 00:39:12 -0400

    Resolve trainner's commentaries

--- Nestor/my-dictionary.py
+++ Nestor/my-dictionary.py
@@ -1,6 +1,9 @@
 my_dictionary = {1: 'lorem', 2: 'ipsum', 3: 'dolor', 4: 'sit', 5: 'amet', 6: 'consectetur'}
 
-
+"""
+This method return dictionary size.
+:return: type int
+"""
 def get_size():
     size = 0
     for element in my_dictionary:
@@ -8,28 +11,51 @@ def get_size():
     return size
 
 
+"""
+This method return a list sorted from dictionary keys.
+:return: type ordered list
+"""
 def get_keys():
     return sorted(my_dictionary.keys())
 
 
+"""
+This method return a list sorted from dictionary values.
+:return: type ordered list
+"""
 def get_values():
     return sorted(my_dictionary.values())
 
 
+"""
+This method return a string from dictionary { key: value } format.
+:return: type string with format
+"""
 def print_dictionary():
     return str(my_dictionary)
 
 
+"""
+This method print ever single pairs key: value.
+"""
 def another_print():
     for key in my_dictionary:
         print(key, ':', my_dictionary[key])
     return
 
 
+"""
+This method return True if key exists in dictionary, False if not.
+:return: type boolean
+"""
 def is_key(key):
     return key in my_dictionary.keys()
 
 
+"""
+This method return True if value exists in dictionary, False if not.
+:return: type boolean
+"""
 def is_value(value):
     return value in my_dictionary.values()
 
