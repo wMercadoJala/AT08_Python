b9d3f585a1982823d4a880980ad0e39d2406cc3a
Rodrigo Menacho <rmenacho21@gmail.com>
2019-02-01 11:00:55 -0400

    created dictionary

--- definitions.py
+++ definitions.py
@@ -2,4 +2,10 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 SCHEMA_CREATION = os.path.join(ROOT_DIR, 'team_one_behave/schemas/projectcreation.schema.todoly.json')
-SCHEMA_ACCOUNT = os.path.join(ROOT_DIR, 'team_one_behave\\schemas\\pivotal_tracker\\account.schema.pivotal.json')
+SCHEMA_ACCOUNT = os.path.join(ROOT_DIR, 'team_one_behave\\schemas\\pivotal_tracker\\account.schema.json')
+
+schema = {
+    'Account': os.path.join(ROOT_DIR, 'team_one_behave/schemas/pivotal_tracker/account.schema.json'),
+    'Project': os.path.join(ROOT_DIR, 'team_one_behave/schemas/pivotal_tracker/project.schema.json'),
+    'Projects': os.path.join(ROOT_DIR, 'team_one_behave/schemas/pivotal_tracker/projects.schema.json')
+}
