c1d138b7bc05ec2531679f1df284efec469cd9f7
Rodrigo Menacho <rmenacho21@gmail.com>
2019-02-01 11:05:47 -0400

    Refactor code

--- definitions.py
+++ definitions.py
@@ -1,11 +1,12 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-SCHEMA_CREATION = os.path.join(ROOT_DIR, 'team_one_behave/schemas/projectcreation.schema.todoly.json')
-SCHEMA_ACCOUNT = os.path.join(ROOT_DIR, 'team_one_behave\\schemas\\pivotal_tracker\\account.schema.json')
 
-schema = {
+schema_pivotal = {
     'Account': os.path.join(ROOT_DIR, 'team_one_behave/schemas/pivotal_tracker/account.schema.json'),
     'Project': os.path.join(ROOT_DIR, 'team_one_behave/schemas/pivotal_tracker/project.schema.json'),
     'Projects': os.path.join(ROOT_DIR, 'team_one_behave/schemas/pivotal_tracker/projects.schema.json')
+}
+schema_todoly = {
+    'Creation': os.path.join(ROOT_DIR, 'team_one_behave/schemas/projectcreation.schema.todoly.json')
 }
--- team_one_behave/features/project/pivotal_tracker/projects_privotal.feature
+++ team_one_behave/features/project/pivotal_tracker/projects_privotal.feature
@@ -10,7 +10,7 @@ Feature: Get Projects
     When I send the request
     Then I get a "200" status code as response
 
-    Scenario: Get Projects Memberships
+  Scenario: Get Projects Memberships
     Given I set up a "GET" request to "/projects/2242689/memberships" endpoint
     When I send the request
     Then I get a "200" status code as response
