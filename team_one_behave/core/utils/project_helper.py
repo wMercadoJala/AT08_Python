e3a62f518313db42a713aabf08a72a69d12d012a
Rodrigo Menacho <rmenacho21@gmail.com>
2019-02-01 14:30:03 -0400

    advance

---
+++ team_one_behave/core/utils/project_helper.py
@@ -0,0 +1,12 @@
+# from team_one_behave.core.rest_client.request_manager import *
+import random
+import string
+
+
+class ProjectHelper():
+    @staticmethod
+    def create_project(context):
+        # client = RequestManager()
+        project_name = "".join(random.choices(string.ascii_letters + string.digits, k=10))
+
+
