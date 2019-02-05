d96878bb03fef06201a02f38fef1f4e765da6779
Nestor Otondo <nestor.otondo@fundacion-jala.org>
2019-02-05 09:47:19 -0400

    Refactor helpers

--- Pivotal/features/environment.py
+++ Pivotal/features/environment.py
@@ -3,8 +3,10 @@ from Pivotal.core.utils.project_helper import ProjectHelper
 
 def before_feature(context, feature):
     if 'create_project' in feature.tags:
-        ProjectHelper.create_project('Test create project')
+        context.project_id = ProjectHelper.create_project('Test create project')
+
 
 def before_scenario(context, scenario):
+    print('Project ID:', context.project_id)
     if 'create_webhook' in scenario.tags:
         ProjectHelper.create_webhook('https://elvillano.com')
